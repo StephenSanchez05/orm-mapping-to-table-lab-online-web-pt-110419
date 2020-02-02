f451d8fc68e2941821778bdefc0a3c446ae52c91
Stephen Sanchez <stephensanchez05@gmail.com>
2020-02-02 22:58:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,13 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    album TEXT
     )
+    SQL
+    DB[:conn].execute(sql)
+  end 
     
   
 end
